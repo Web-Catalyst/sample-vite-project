10d776216e2c131b6add5e77ce8f4a4024097a24
Mihir Paldhikar <imihirpaldhikar@gmail.com>
2024-02-25 22:03:03 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Sample Vite Project
+
+This repository is created to test deployments of vite projects.
---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <main className="min-h-screen flex">
+      <div className="m-auto">
+        <div className="flex flex-col space-y-5">
+          <h1 className="text-3xl font-bold">WebCatalyst</h1>
+          <p className="text-lg font-medium">This is a sample Vite Project.</p>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
