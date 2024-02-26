0c602a1593394c83238111a7195a51281c2058d4
Mihir Paldhikar <imihirpaldhikar@gmail.com>
2024-02-26 14:06:52 +0530

    feat: Add About page for routing tests.

---
+++ src/About.tsx
@@ -0,0 +1,11 @@
+import {JSX} from "react";
+
+export default function About(): JSX.Element {
+    return (
+        <div className={"min-h-screen flex"}>
+            <div className={"m-auto"}>
+                <h2 className={"text-2xl font-bold"}>About</h2>
+            </div>
+        </div>
+    )
+}
